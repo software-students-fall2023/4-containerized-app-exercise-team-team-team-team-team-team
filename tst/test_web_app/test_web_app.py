e3d1efbd937de14e825419e4dc54930e69d1edb8
Jhon Kim <yk2606@nyu.edu>
2023-11-27 16:27:31 -0500

    wrote test cases relevant to new web_app.py

--- tst/test_web_app/test_web_app.py
+++ tst/test_web_app/test_web_app.py
@@ -1,20 +1,40 @@
 """Does tests for ze program"""
 import os
 import pytest
+import json
+import base64
 from dotenv import load_dotenv
-from src.web_app.web_app import connect_to_db
-from src.machine_learning_client.main import detect_encoding
-
+from src.machine_learning_client.main import detect_encoding, connect_to_db 
 
 load_dotenv()
 DATABASE_CONNECTION_STRING = os.getenv("DATABASE_CONNECTION_STRING")
 
 
 @pytest.fixture
-def database_connection_string():
-    """DB connection string"""
-    return DATABASE_CONNECTION_STRING
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
 
+def test_dashboard_route(client):
+    """Test the dashboard route."""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"homepage.html" in response.data  
+
+def test_data_collection_get_route(client):
+    """Test the data collection GET route."""
+    response = client.get("/data_collection")
+    assert response.status_code == 200
+    assert b"data_collection.html" in response.data
+
+def test_data_collection_post_route(client):
+    """Test the data collection POST route."""
+    image_data = base64.b64encode(b'Test Image Data').decode('utf-8')
+    response = client.post("/data_collection", json={'image': f'data:image/png;base64,{image_data}'})
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert 'Image uploaded successfully' in data['message']
 
 def test_connection_to_db_successful(db_connection_string):
     """Tests connection to DB"""
@@ -22,13 +42,11 @@ def test_connection_to_db_successful(db_connection_string):
     assert ml_data is not None
     assert db is not None
 
-
 def test_collection_and_database_exist(db_connection_string):
     """Tests db connection and existence"""
     ml_data, db = connect_to_db(db_connection_string)
     assert ml_data.name == "DataForMachineLearning"
     assert db.name == "MLData"
-
 
 def test_detect_encoding1():
     """Tests detect encoding 1"""
