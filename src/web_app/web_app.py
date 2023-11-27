c6e43fc5b6552b22932c506966e340bf1af7ba77
Jhon Kim <yk2606@nyu.edu>
2023-11-27 15:07:54 -0500

    fixed all formating issues

--- src/web_app/web_app.py
+++ src/web_app/web_app.py
@@ -1,14 +1,16 @@
-from dotenv import load_dotenv
-from flask import Flask, render_template, request, jsonify
-import pymongo
+"""All necessary imports to run web_app.py"""
 import os
 import logging
-import base64 
+import base64
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, jsonify
+
+# import pymongo
+
 
 logging.basicConfig(level=logging.INFO)
 load_dotenv()
 # DATABASE_CONNECTION_STRING = os.getenv('DATABASE_CONNECTION_STRING')
-#print("hello world")
 log = logging.getLogger()
 app = Flask(__name__)
 
@@ -31,39 +33,49 @@ app = Flask(__name__)
 log = logging.getLogger()
 # collection, dataBase = connectToDB(DATABASE_CONNECTION_STRING)
 
+
 @app.route("/")
 def view_dashboard():
     """renders dashboard"""
     return render_template("homepage.html")
 
-app.route('/data_collection', methods=['GET'])
-def data_collection_get():
-    return render_template('data_collection.html')
 
-@app.route('/data_collection', methods=['POST'])
+@app.route("/data_collection", methods=["GET"])
+def data_collection_get():
+    """gets page for data collection"""
+    return render_template("data_collection.html")
+
+
+@app.route("/data_collection", methods=["POST"])
 def data_collection_post():
+    """handles post request of photo that was collected on page"""
     try:
-        image_data = request.json['image']  # extracting base64 image data
-        image_binary = base64.b64decode(image_data.split(',')[1])  # decode the image
+        image_data = request.json["image"]  # extracting base64 image data
+        image_binary = base64.b64decode(image_data.split(",")[1])  # decode the image
 
         # determining file path
         script_dir = os.path.dirname(__file__)  # directory of the script
-        target_dir = os.path.join(script_dir, '..', '..', 'images')  # navigating up 'images' folder
-        # os.makedirs(target_dir, exist_ok=True)  -> creating  directory if it doesn't exist, not sure if this might be an issue on other machines...
+        target_dir = os.path.join(
+            script_dir, "..", "..", "images"
+        )  # navigating up 'images' folder
+        # os.makedirs(target_dir, exist_ok=True)
+        # -> creating directory if it doesn't exist, might be an issue on other machines
 
         # defining file name, it doesn't need to be unique if docker file clears image folder
-        file_path = os.path.join(target_dir, 'uploaded_image.png') 
+        file_path = os.path.join(target_dir, "uploaded_image.png")
 
         # writing image data into a file
-        with open(file_path, 'wb') as file:
+        with open(file_path, "wb") as file:
             file.write(image_binary)
 
-        return jsonify({"message": "Image uploaded successfully", "file_path": file_path})
+        return jsonify(
+            {"message": "Image uploaded successfully", "file_path": file_path}
+        )
 
     except Exception as e:
-        logging.error(f"Error uploading image: {e}")
+        logging.error("Error uploading image: %s", e)
         return jsonify({"error": "Error uploading image"}), 500
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
