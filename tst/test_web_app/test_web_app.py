e4628be14ceab3d6ffdcc29c638b61f270452f21
Jhon Kim <yk2606@nyu.edu>
2023-11-30 19:39:20 -0500

    finished writing test cases

--- tst/test_web_app/test_web_app.py
+++ tst/test_web_app/test_web_app.py
@@ -1,12 +1,12 @@
 """Does tests for ze program"""
 import os
-
-# import json
-# import base64
+import pytest
+import json
+import base64
 from dotenv import load_dotenv
-from src.web_app import connect_to_db
-from src.main import detect_encoding
-
+from src.web_app import connect_to_db, app, get_emotion_aggregate
+from src.main import detect_encoding, get_image
+from unittest.mock import patch, MagicMock
 
 load_dotenv()
 DATABASE_CONNECTION_STRING = os.getenv("DATABASE_CONNECTION_STRING")
@@ -49,37 +49,54 @@ def test_detect_encoding3():
     detect_encoding("tst/test_web_app/tests/testFile1.txt")
 
 
-# @pytest.fixture
-# def client():
-#     app.config["TESTING"] = True
-#     with app.test_client() as client:
-#         yield client
+def test_get_image():
+    """Test if get_image function returns a valid file path."""
+    image_path = get_image()
+    assert os.path.exists(image_path)
+    assert os.path.isfile(image_path)
 
 
-# def test_data_collection_get_route(client):
-#     """Test the data collection GET route."""
-#     response = client.get("/data_collection")
-#     assert response.status_code == 200
-#     assert b"data_collection.html" in response.data
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
 
 
-# def test_data_collection_post_route(client):
-#     """Test the data collection POST route."""
-#     image_data = base64.b64encode(b"Test Image Data").decode("utf-8")
-#     response = client.post(
-#         "/data_collection", json={"image": f"data:image/png;base64,{image_data}"}
-#     )
-#     assert response.status_code == 200
-#     data = json.loads(response.data)
-#     assert "Image uploaded successfully" in data["message"]
+def test_homepage_get_route(client):
+    """Test the homepage GET route."""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Read my facial expression" in response.data
 
 
-# def test_data_collection_post_route(client):
-#     """Test the data collection POST route."""
-#     image_data = base64.b64encode(b"Test Image Data").decode("utf-8")
-#     response = client.post(
-#         "/data_collection", json={"image": f"data:image/png;base64,{image_data}"}
-#     )
-#     assert response.status_code == 200
-#     data = json.loads(response.data)
-#     assert "Image uploaded successfully" in data["message"]
+def test_data_collection_get_route(client):
+    """Test the data collection GET route."""
+    response = client.get("/data_collection")
+    assert response.status_code == 200
+    assert b"Capture Emotion" in response.data
+
+
+def test_data_collection_post_route(client):
+    """Test the data collection POST route."""
+    image_data = base64.b64encode(b"Test Image Data").decode("utf-8")
+    response = client.post(
+        "/data_collection", json={"image": f"data:image/png;base64,{image_data}"}
+    )
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert "Image uploaded successfully" in data["message"]
+
+
+def test_get_emotion_aggregate():
+    with patch("src.web_app.connect_to_db") as mock_connect_to_db:
+        # mocking database query
+        mock_collection = MagicMock()
+        mock_collection.find.return_value = [
+            {"emotion": "happy"},
+            {"emotion": "sad"},
+            {"emotion": "happy"},
+        ]
+        mock_connect_to_db.return_value = (mock_collection, None)
+        emotion_data = get_emotion_aggregate()
+        assert emotion_data == {"happy": 2, "sad": 1}
